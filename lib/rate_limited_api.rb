cab13c409a48fbebfd00ba5c214fc391f67e690a
Yann Marquet <ymarquet@gmail.com>
2013-11-26 14:25:16 +0000

    spilt api and limiter in their own file

--- lib/rate_limited_api.rb
+++ lib/rate_limited_api.rb
@@ -1,5 +1,6 @@
 require "rate_limited_api/version"
 require "rate_limited_api/config"
+require "rate_limited_api/api"
 require "rate_limited_api/limiter"
 require "active_support/core_ext/string"
 require "active_support/core_ext/numeric"
--- lib/rate_limited_api/limiter.rb
+++ lib/rate_limited_api/limiter.rb
@@ -2,27 +2,6 @@ module RateLimitedApi
 
   class RateLimitReached < StandardError; end
 
-  class Api
-    def initialize(object, limited_methods, limiter)
-      @object          = object
-      @limited_methods = limited_methods.map(&:to_sym)
-      @limiter         = limiter
-    end
-
-    def method_missing(method_name, *args)
-      if @object.respond_to?(method_name)
-        @limiter.incr if @limited_methods.include?(method_name.to_sym)
-        @object.send(method_name, *args)
-      else
-        super
-      end
-    end
-
-    def respond_to_missing?(method_name, include_private = false)
-      @object.respond_to?(method_name) || super
-    end
-  end
-
   class Limiter
 
     def initialize(api_id, rate, time_unit)
